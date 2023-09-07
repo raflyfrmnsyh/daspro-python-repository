70aa65e39e1d71ece1ad2e1e15f73248d3b83b73
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2023-09-07 20:23:24 +0700

    program check kelulusan

---
+++ kelulusan.py
@@ -0,0 +1,9 @@
+# Program Aplikasi untuk check kelulusan nilai daspro
+print("\nPROGRAM CHECK KELULUSAN NILAI DASPRO\n")
+
+nilai = int(input("Masukan nilai anda :"))
+
+if nilai >= 60 :
+    print("Anda di nyatakan Lulus!!")
+else :
+    print("Anda di nyatakan tidak Lulus")

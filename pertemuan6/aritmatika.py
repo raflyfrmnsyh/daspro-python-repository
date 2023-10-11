1dc0142f1167558138d15baf2a6782e3d128dd18
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2023-10-11 15:04:51 +0700

    menambah pertemuan 6

--- pertemuan6/aritmatika.py
+++
@@ -1,5 +0,0 @@
-print(17 / 5)
-print(17 // 5)
-
-print(10 % 3)
-print(2 ** 5)
---
+++ pertemuan6/materi.py
@@ -0,0 +1,45 @@
+# OPERASI PADA PYTHON
+# ===========================================================
+
+# OPERASI ARITMATIKA, UNTUK MELAKUKAN PERHITUNGAN
+# OPERASI PERKONDISIAN, UNTUK MELAKUKAN PENGKONDISIAN
+# OPERASI LOGIKA, DIGUNAKAN PADA SAAT AKAN MENGECHECK KONDISI
+# OPERASI PENUGASAN, DIGUNAKAN UNTUK MELAKUKAN PENUGASAN
+# ===========================================================
+
+# OPERASI ARITMATIKA
+
+a = 10
+b = 15
+
+tambah  = a + b
+kurang  = a - b
+kali    = a * b
+bagi    = a / b
+modulus = a % b
+division = a // b
+
+
+# OPERASI PENGKONDISIAN 
+# IF
+if (a < b): 
+    print(a, "Lebih kecil dari pada", b)
+# ELSE
+else: 
+    print(a, "Tidak lebih kecil dari pada", b)
+
+# OPERASI LOGIKA 
+# == (Equals)
+# != (NotEquals)
+# >  (Lebih besar)
+# <  (Lebih kecil)
+# <= (Lebih kecil sama dengan)
+# >= (Lebih besar sama dengan)
+
+
+# OPERASI PENUGASAN 
+# += (Penjumlahan)
+# -= (Pengurangan)
+# *= (Perkalian)
+# /= (Pembagian)
+# %= (Modulus)
